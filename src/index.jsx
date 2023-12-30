5b98829f9f2da2a13be7cc951480156c3d0419fc
Timo Laakkonen <timo.laakkonen@suonentieto.fi>
2023-12-30 02:52:34 +0200

    updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import App from './App';
-import './styles.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './styles.css'
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('app'))
